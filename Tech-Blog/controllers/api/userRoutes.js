8c523ac5ea879b9042009269024d48b025aa3940
Maleeha <162085935+maleehali@users.noreply.github.com>
2024-11-11 20:02:00 -0500

    editted paths for the hundredth time

--- Tech-Blog/controllers/api/userRoutes.js
+++ Tech-Blog/controllers/api/userRoutes.js
@@ -1,6 +1,6 @@
 // controllers/api/userRoutes.js
 const router = require('express').Router();
-const { User } = require('../../../models');
+const { User } = require('../../models'); // Adjusted to match the relative path to models directory
 const bcryptjs = require('bcryptjs');
 
 // Signup route
