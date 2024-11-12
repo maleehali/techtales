ab0083a65f34213ad8334614710625a9499b7f1e
Maleeha <162085935+maleehali@users.noreply.github.com>
2024-11-11 19:31:05 -0500

    Fixed models path for deployment on Render

--- Tech-Blog/controllers/api/userRoutes.js
+++ Tech-Blog/controllers/api/userRoutes.js
@@ -1,6 +1,6 @@
 // controllers/api/userRoutes.js
 const router = require('express').Router();
-const { User } = require('../../../models');
+const { User } = require('../../models');
 const bcrypt = require('bcrypt');
 
 // Signup route
