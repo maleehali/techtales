ead6344c5c30b3eae2b942ec085eef1e4f8161f5
Maleeha <162085935+maleehali@users.noreply.github.com>
2024-11-12 17:10:56 -0500

    reorganized files

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,7 @@
 const router = require('express').Router();
 const userRoutes = require('./api/userRoutes');
 const postRoutes = require('./api/postRoutes');
-const homeRoutes = require('./homeRoutes');
+const homeRoutes = require('./api/homeRoutes');
 
 router.use('/api/users', userRoutes);
 router.use('/api/posts', postRoutes);
